f7cf5e9634baef8e799c191c64ff01d6ffb51f50
yayashn <78495264+yayashn@users.noreply.github.com>
2022-10-13 12:12:14 +0100

    new answers

--- client/src/config/questions.ts
+++ client/src/config/questions.ts
@@ -1,5 +1,5 @@
 import { QuestionData } from "@/types";
-import questionsJSON from "./questions.json";
+import questionsJSON from "./questions2.json";
 
 export interface Questions {
   [category: string]: QuestionData[]
