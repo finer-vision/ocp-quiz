da5b61306e6f40d20a4cfbfd6730ffd2fee7dbc4
Enijar <TheEnijar@gmail.com>
2022-10-18 09:19:56 -0700

    fix: win times

--- server/src/config.ts
+++ server/src/config.ts
@@ -7,8 +7,6 @@ type Config = {
   winningCodes: WinningCode[];
 };
 
-const localTimeZoneOffset = 0;
-
 const config: Config = {
   usedCodeIdsFilePath: path.resolve(
     __dirname,
@@ -22,49 +20,37 @@ const config: Config = {
     {
       id: 1,
       digits: [9, 6, 8, 1],
-      validAfter: new Date(
-        Date.UTC(2022, 9, 18, 12 + localTimeZoneOffset, 0, 0)
-      ),
+      validAfter: new Date(2022, 9, 18, 12, 0, 0),
     },
     // Day 1 – afternoon win
     {
       id: 2,
       digits: [2, 8, 4, 1],
-      validAfter: new Date(
-        Date.UTC(2022, 9, 18, 17 + localTimeZoneOffset, 0, 0)
-      ),
+      validAfter: new Date(2022, 9, 18, 17, 0, 0),
     },
     // Day 2 – morning win
     {
       id: 3,
       digits: [1, 0, 1, 5],
-      validAfter: new Date(
-        Date.UTC(2022, 9, 19, 11 + localTimeZoneOffset, 0, 0)
-      ),
+      validAfter: new Date(2022, 9, 19, 11, 0, 0),
     },
     // Day 2 – afternoon win
     {
       id: 4,
       digits: [8, 4, 8, 8],
-      validAfter: new Date(
-        Date.UTC(2022, 9, 19, 15 + localTimeZoneOffset, 0, 0)
-      ),
+      validAfter: new Date(2022, 9, 19, 15, 0, 0),
     },
     // Day 3 – morning win
     {
       id: 5,
       digits: [2, 6, 6, 4],
-      validAfter: new Date(
-        Date.UTC(2022, 9, 20, 9 + localTimeZoneOffset, 30, 0)
-      ),
+      validAfter: new Date(2022, 9, 20, 9, 30, 0),
     },
     // Day 3 – afternoon win
     {
       id: 6,
       digits: [4, 8, 5, 0],
-      validAfter: new Date(
-        Date.UTC(2022, 9, 20, 12 + localTimeZoneOffset, 0, 0)
-      ),
+      validAfter: new Date(2022, 9, 20, 12, 0, 0),
     },
   ],
 };
