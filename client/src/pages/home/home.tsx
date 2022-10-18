47ed3d219097f7b0c928e307039bcb4c2fcba068
Enijar <TheEnijar@gmail.com>
2022-10-18 09:10:09 -0700

    fix: black screen issue

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -1,10 +1,8 @@
-import React, { useContext, useEffect } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import config from "@/config/config";
 import {
   HomeButton,
   HomeContent,
-  HomeIntroVideo,
   HomeVideo,
   HomeWrapper,
   Sticker,
@@ -16,41 +14,55 @@ import BuildingsParallax from "@/components/buildings-parallax/buildings-paralla
 import TitleSign from "@/components/title-sign/title-sign";
 import { useAnimationControls } from "framer-motion";
 
-export default function Home(props: {resetTimer: () => void}) {
+export default function Home(props: { resetTimer: () => void }) {
   const [state, setState] = React.useState<HomeState>(HomeState.initial);
 
   const navigate = useNavigate();
   const controls = useAnimationControls();
 
   React.useEffect(() => {
-    if(state === HomeState.intro) {
+    if (state === HomeState.intro) {
       controls.start({
-        x: '100vw',
-        transition: {}
-      })
+        x: "100vw",
+        transition: {},
+      });
       var timeout = setTimeout(() => {
-          navigate("/category-selector");
+        navigate("/category-selector");
       }, 2000);
     }
 
     return () => clearTimeout(timeout);
   }, [state, navigate]);
-  
+
+  const [key, setKey] = React.useState(0);
+
+  const refresh = React.useCallback(() => {
+    setKey((key) => {
+      let nextKey = key + 1;
+      if (nextKey > 1000) {
+        nextKey = 0;
+      }
+      return nextKey;
+    });
+  }, []);
 
   return (
-    <HomeWrapper state={state} onClick={props.resetTimer}>
+    <HomeWrapper key={key} state={state} onClick={props.resetTimer}>
       <Events />
       <HomeContent onClick={() => setState(HomeState.intro)}>
         <Header />
-        <Sticker animate={controls} src="./assets/sticker.png"/>
+        <Sticker animate={controls} src="./assets/sticker.png" />
         <TitleSign />
         {state === HomeState.initial && (
-          <HomeButton
-            src="./assets/start.png"
-            alt="START"
-          />
+          <HomeButton src="./assets/start.png" alt="START" />
         )}
-      <HomeVideo animate={controls} src="./assets/videos/1.1.webm" autoPlay muted loop/>
+        <HomeVideo
+          animate={controls}
+          src="./assets/videos/1.1.webm"
+          autoPlay
+          muted
+          onEnded={refresh}
+        />
       </HomeContent>
       <BuildingsParallax />
     </HomeWrapper>
